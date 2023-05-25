2092c65a1ff4dc050d3d821c76873a65ceb5912e
DawKapi82 <dawid_kapi82@hotmail.com>
2023-05-25 12:15:17 +0000

    update from battelship1 to battelship2 on github

--- README.md
+++ README.md
@@ -1,32 +1,46 @@
-![CI logo](https://codeinstitute.s3.amazonaws.com/fullstack/ci_logo_small.png)
+Battleship Game
 
-Welcome,
+The Battleship Game is a Python application that allows users to play the classic game of Battleship against the computer. It also provides an online version of the game accessible at https://battelship.herokuapp.com/.
 
-This is the Code Institute student template for deploying your third portfolio project, the Python command-line project. The last update to this file was: **March 14, 2023**
+Purpose
 
-## Reminders
+The purpose of this application is to recreate the popular Battleship game online web version. It offers a fun and challenging gaming experience where players can test their skills in tactical thinking and decision-making. By playing the Battleship Game, users can enjoy the nostalgia of the original game while also experiencing the convenience of an online version.
 
-- Your code must be placed in the `run.py` file
-- Your dependencies must be placed in the `requirements.txt` file
-- Do not edit any of the other files or your code may not deploy properly
+Key Features
 
-## Creating the Heroku app
+Player vs. computer gameplay: Compete against an AI opponent in an engaging naval battle.
+Random ship placement: The game randomly places four ships on each player's board, adding an element of unpredictability to each game.
+Turn-based gameplay: Take turns guessing the coordinates to target the opponent's ships, creating a strategic and suspenseful experience.
+Real-time feedback: The game provides immediate feedback on hit or miss, allowing players to adjust their strategy accordingly.
+Interactive interface: The game utilizes a simple and user-friendly interface to facilitate smooth gameplay.
+How to Play
 
-When you create the app, you will need to add two buildpacks from the _Settings_ tab. The ordering is as follows:
+To play the Battleship Game:
 
-1. `heroku/python`
-2. `heroku/nodejs`
+Visit https://battelship.herokuapp.com/ in your web browser.
+Follow the on-screen instructions to start the game.
+Guess the row and column coordinates to target your opponent's ships.
+The game will display the result of your hit, whether it's a hit or a miss.
+The computer opponent will take its turn and display the result.
+Continue taking turns until one player successfully sinks all the opponent's ships.
+At the end of the game, the winner will be announced.
+You can choose to play another round or exit the game.
+Value to Users The Battleship Game provides several benefits and value to its users:
 
-You must then create a _Config Var_ called `PORT`. Set this to `8000`
+Entertainment: The game offers an enjoyable and nostalgic gaming experience that allows users to relive the excitement of playing Battleship.
 
-If you have credentials, such as in the Love Sandwiches project, you must create another _Config Var_ called `CREDS` and paste the JSON into the value field.
+Skill Development: By playing Battleship, users can enhance their strategic thinking, decision-making, and problem-solving skills.
 
-Connect your GitHub repository and deploy as normal.
+Convenience: The online version of the game provides the flexibility to play anytime and anywhere, without the need for Python installation or local setup.
 
-## Constraints
+User-Friendly Interface: The game features an intuitive and interactive interface that makes it easy for users to navigate and play the game.
 
-The deployment terminal is set to 80 columns by 24 rows. That means that each line of text needs to be 80 characters or less otherwise it will be wrapped onto a second line.
+Engaging Gameplay: The turn-based gameplay and real-time feedback create a suspenseful and immersive gaming experience, keeping users entertained and challenged.
 
----
+Variety and Randomness: With random ship placement, each game offers a unique challenge, ensuring that no two games are the same.
 
-Happy coding!
+Multiplayer Option: The online version allows users to play against friends or other online players, adding a social and competitive element to the game.
+
+Notes
+
+The application is released under the MIT License, granting users the freedom to use, modify, and distribute the software.
--- run.py
+++ run.py
@@ -1,3 +1,184 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from random import randint
+
+"""list for history of player moves"""
+history = []
+
+"""generate array with 4 ships in random places"""
+
+
+def generate_arr():
+    arr = []
+    for j in range(0, 5):
+        arr.append([0 for i in range(0, 5)])
+
+    for i in range(4):
+        ri = randint(0, 4)
+        ci = randint(0, 4)
+        while arr[ri][ci] == 1:
+            ri = randint(0, 4)
+            ci = randint(0, 4)
+
+        arr[ri][ci] = 1
+
+    return arr
+
+
+"""print the board on the screen function"""
+
+
+def display_arr(player_arr, computer_arr):
+    print("Player's board:")
+    for r in player_arr:
+        rs = ""
+        for el in r:
+            if el == 0:
+                rs += ". "
+            else:
+                rs += "@ "
+        print(rs)
+
+    print("Computer's board:")
+    for r in computer_arr:
+        rs = ""
+        for el in r:
+            if el == 0:
+                rs += ". "
+            else:
+                rs += "X "
+        print(rs)
+
+
+"""hit opponents board function"""
+
+
+def hit(arr, ri, ci):
+    if arr[ri][ci] == 1:
+        arr[ri][ci] = 0
+        return 1
+    else:
+        return 0
+
+
+"""check if board contains only zeros function"""
+
+
+def empty(arr):
+    return sum([sum(row) for row in arr]) == 0
+
+
+"""check if coordinates are valid and they
+are not in history already function"""
+
+
+def check_coords(ri, ci):
+    if ri < 0 or ri > 4 or ci < 0 or ci > 4:
+        print("Insert values in the range from 0 to 4")
+        return False
+
+    if (ri, ci) in history:
+        print("These coordinates were already hit")
+        return False
+
+    return True
+
+
+"""players points vars"""
+player_points = 0
+computer_points = 0
+
+print("Please enter your name:")
+name = input()
+
+while name.strip() == "":
+    print("Name cannot be empty. Please enter your name:")
+    name = input()
+
+"""generate and display player's board"""
+player_arr = generate_arr()
+
+"""generate and display computer's board"""
+computer_arr = generate_arr()
+
+"""variable determining the main loop of the game"""
+game = True
+
+"""if game is True, repeat rounds"""
+while game:
+    display_arr(player_arr, computer_arr)
+
+    ri = -1
+    ci = -1
+
+    """player inserts coordinates until they are valid"""
+    while not check_coords(ri, ci):
+        print("Guess a row (0-4):")
+        try:
+            ri = int(input())
+        except ValueError:
+            print("Invalid input. Please enter an integer value.")
+            continue
+
+        print("Guess a column (0-4):")
+        try:
+            ci = int(input())
+        except ValueError:
+            print("Invalid input. Please enter an integer value.")
+            continue
+
+    """player's hit"""
+    print("Player guessed: (", ri, ",", ci, ")")
+    points = hit(computer_arr, ri, ci)
+    """add coordinates to history"""
+    history.append((ri, ci))
+
+    """display result of hit"""
+    if points == 1:
+        print("Player hit the ship")
+    else:
+        print("Player missed")
+
+    """add points"""
+    player_points += points
+
+    """random computer's move"""
+    ri = -1
+    ci = -1
+
+    while not check_coords(ri, ci):
+        ri = randint(0, 4)
+        ci = randint(0, 4)
+
+    print("Computer guessed: (", ri, ",", ci, ")")
+
+    """computer's hit"""
+    points = hit(player_arr, ri, ci)
+
+    """display result of hit"""
+    if points == 1:
+        print("Computer hit the ship")
+    else:
+        print("Computer missed")
+
+    """add points"""
+    computer_points += points
+
+    """display summary of the round"""
+    print("---------------------------------")
+    print("After this round, the scores are:")
+    print(name, ":", player_points, "points. Computer:", computer_points,
+          "points")
+    print("---------------------------------")
+
+    """check if someone won the game and end the game"""
+    if empty(player_arr):
+        print("Computer won the game!")
+        game = False
+    elif empty(computer_arr):
+        print("Player", name, "won the game!")
+        game = False
+
+    """player decides to play or stop the game"""
+    print("Press any key to continue or 'no' to quit")
+    command = input().lower()
+    if command == "no":
+        game = False
